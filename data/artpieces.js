9153c2b176cc13e7ff92c645c1fe2cdf6fe998df
kasi <danielkaser@mailbox.org>
2023-12-14 16:55:25 +0100

    create data/artpieces.js

---
+++ data/artpieces.js
@@ -0,0 +1,32 @@
+export const data = [
+  // ***** Demo-Data-Start
+  //   {
+  //     slug: "orange-red-and-green",
+  //     artist: "Carlo Calucci",
+  //     name: "Orange Red and Green Abstract Painting",
+  //     imageSource:
+  //       "https://carlo-api.vercel.app/assets/art/pinkrevolution-72dpi.jpg",
+  //     year: "2018",
+  //     genre: "Abstract Painting",
+  //     colors: ["#0f5855", "#e6ba15", "#b42011", "#cec4c6", "#d5561f"],
+  //     dimensions: { height: 2432, width: 1920, type: "jpg" },
+  //   },
+  // ***** Demo-Data-End
+  {
+    slug: "pink-revolution",
+    artist: "Carlo Kozlovic",
+    name: "Pink Revolution",
+    imageSource: "https://carlo-api.vercel.app/assets/art/pink-revolution.jpg",
+    year: "2012",
+    genre: "Abstract Painting",
+    colors: ["#1c2026", "#fe449b", "#681703", "#b9ddfe", "#f6f5e1"],
+    dimensions: { height: 3116, width: 4620, type: "jpg" },
+
+    resolutionDPI: 72,
+    technique: "copics-crayon-acryl-neon",
+    originalRepro: "repro",
+    price: 15320,
+    currency: "Euro",
+    dimensionsCM: { height: 27.7, width: 42 },
+  },
+];
