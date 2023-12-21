1ff5b8470467d1e381fd4890aac835acb9bb2733
kasi <danielkaser@mailbox.org>
2023-12-21 12:45:51 +0100

    add getter to objects to calculate imageSource from given data

--- data/artpieces.js
+++ data/artpieces.js
@@ -1,22 +1,10 @@
+const imagePath = "https://carlo-api.vercel.app/assets/art/";
+
 export const data = [
-  // ***** Demo-Data-Start
-  //   {
-  //     slug: "orange-red-and-green",
-  //     artist: "Carlo Calucci",
-  //     name: "Orange Red and Green Abstract Painting",
-  //     imageSource:
-  //       "https://carlo-api.vercel.app/assets/art/pinkrevolution-72dpi.jpg",
-  //     year: "2018",
-  //     genre: "Abstract Painting",
-  //     colors: ["#0f5855", "#e6ba15", "#b42011", "#cec4c6", "#d5561f"],
-  //     dimensions: { height: 2432, width: 1920, type: "jpg" },
-  //   },
-  // ***** Demo-Data-End
   {
     slug: "pink-revolution",
     artist: "Carlo Kozlovic",
     name: "Pink Revolution",
-    imageSource: "https://carlo-api.vercel.app/assets/art/pink-revolution.jpg",
     year: "2012",
     genre: "Abstract Painting",
     colors: ["#1c2026", "#fe449b", "#681703", "#b9ddfe", "#f6f5e1"],
@@ -28,12 +16,15 @@ export const data = [
     price: 15320,
     currency: "Euro",
     dimensionsCM: { height: 27.7, width: 42 },
+
+    get imageSource() {
+      return imagePath + this.slug + "." + this.dimensions.type;
+    },
   },
   {
     slug: "shessobuttelfly",
     artist: "Carlo Kozlovic",
     name: "shessobutterfly",
-    imageSource: "https://carlo-api.vercel.app/assets/art/shessobuttelfly.jpg",
     year: "2023",
     genre: "Abstract Painting",
     colors: ["#fcdcf0", "#feefc6", "#fc5a70", "#a06d6c", "#da6b33"],
@@ -45,5 +36,9 @@ export const data = [
     price: 50000,
     currency: "Euro",
     dimensionsCM: { height: 29.7, width: 21 },
+
+    get imageSource() {
+      return imagePath + this.slug + "." + this.dimensions.type;
+    },
   },
 ];
