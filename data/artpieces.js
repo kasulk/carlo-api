148e5b4ef185117f6a03c3df353c7ac2a5f20a5f
kasi <danielkaser@mailbox.org>
2023-12-21 12:56:17 +0100

    reorder properties of artpieces data

--- data/artpieces.js
+++ data/artpieces.js
@@ -2,38 +2,36 @@ const imagePath = "https://carlo-api.vercel.app/assets/art/";
 
 export const data = [
   {
-    slug: "pink-revolution",
-    artist: "Carlo Kozlovic",
     name: "Pink Revolution",
+    artist: "Carlo Kozlovic",
+    slug: "pink-revolution",
     year: "2012",
     genre: "Abstract Painting",
     colors: ["#1c2026", "#fe449b", "#681703", "#b9ddfe", "#f6f5e1"],
     dimensions: { height: 3116, width: 4620, type: "jpg", dpi: 72 },
-
+    dimensionsCM: { height: 27.7, width: 42 },
     technique: "copics-crayon-acryl-neon",
     repro: true,
     price: 15320,
     currency: "Euro",
-    dimensionsCM: { height: 27.7, width: 42 },
 
     get imageSource() {
       return imagePath + this.slug + "." + this.dimensions.type;
     },
   },
   {
-    slug: "shessobuttelfly",
-    artist: "Carlo Kozlovic",
     name: "shessobutterfly",
+    artist: "Carlo Kozlovic",
+    slug: "shessobuttelfly",
     year: "2023",
     genre: "Abstract Painting",
     colors: ["#fcdcf0", "#feefc6", "#fc5a70", "#a06d6c", "#da6b33"],
     dimensions: { height: 1080, width: 806, type: "jpg", dpi: 72 },
-
+    dimensionsCM: { height: 29.7, width: 21 },
     technique: "digital-painting",
     repro: false,
     price: 50000,
     currency: "Euro",
-    dimensionsCM: { height: 29.7, width: 21 },
 
     get imageSource() {
       return imagePath + this.slug + "." + this.dimensions.type;
